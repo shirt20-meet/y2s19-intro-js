6d1d6505c397e508d603ce323ee1098232b94464
myname21-meet <myname21@meet.mit.edu>
2019-07-24 09:45:55 -0400

    addind with jQuery

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var birthYear = parseInt(prompt("What year you were born?"));
+/*var birthYear = parseInt(prompt("What year you were born?"));
 var luckyNumber = parseInt(prompt("What is your lucky number?"));
 var favFruite = "peach";
 var favSubject = "cs";
@@ -14,14 +14,31 @@ var meet = prompt("write three thing ypu like about meet separated by comma and 
 meet = meet.split(", ");
 meet.sort();
 console.log(meet);
-var number = Math.floor((Math.random() * 100) + 1);
-var number1 = prompt("guess a number between 1 - 100");
-if (number>number1) {
-	alert("the number you guessed is to high")
+*/
+
+//var number1 = prompt("guess a number between 1 - 100");
+//if (number>number1) {
+//	alert("the number you guessed is to high")
+//}
+//else if (number<number1){
+//	alert("the number you guessed is to low")
+//}
+//else {
+//	alert("you are right!")
+//}
+var number = Math.floor((Math.random() * 10) + 1);
+
+while (number!=number1){
+	var number1 = prompt("guess a number between 1 - 10");
+	if (number>number1) {
+		alert("the number you guessed is to low")
+	}
+	else if (number<number1){
+		alert("the number you guessed is to high")
+	}
+	else {
+		alert("you are right!")
+	}
 }
-else if (number<number1){
-	alert("the number you guessed is to low")
-}
-else {
-	alert("you are right!")
-}
+
+alert("now after you guessed corectly the number im giving you the permission to use my amazing website")
